e593105d29de0bcbcc7770b5e6ce641bc435f22c
Simon <sema92m@wp.pl>
2022-10-12 18:01:32 +0200

    first commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+/* Creating an array an passing number, questions, options and answers */
+let questions = [
+    {
+        numb: 1,
+        question: "Konstruktor ktory ma wielka wiedza konstruktorska (i nie tylko)",
+        answer: "S.Z.",
+        options: [
+            "P.K.",
+            "S.Z.",
+            "M.C.",
+            "L.R."
+        ]
+    },
+    {
+        numb: 2,
+        question: "Jedyny konstruktor majacy stopien PROFFESORA",
+        answer: "A.W.",
+        options: [
+            "R.S.",
+            "M.S.",
+            "A.A.",
+            "A.W."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Konstruktor ktory ma swoj LAS i 'Indastry' ",
+        answer: "M.S.",
+        options: [
+            "M.S.",
+            "I.D.",
+            "P.B.",
+            "W.L"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Konstruktor ktory zapierdala co dziennie 60 km w jedna strone zeby swoja wiedza wspierac AMS",
+        answer: "L.R.",
+        options: [
+            "A.W.",
+            "R.S.",
+            "M.C.",
+            "L.R."
+        ]
+    },
+    {
+        numb: 5,
+        question: "Konstruktor ktory kupil fotowaltaike za gotowke i bierze tylko 1700zl za granie na msze",
+        answer: "P.K.",
+        options: [
+            "A.W.",
+            "S.Z.",
+            "P.K.",
+            "R.S."
+        ]
+    },
+];
